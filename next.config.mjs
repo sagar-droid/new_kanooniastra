08243f1590efd1e23b675075c4c1b0ba874674aa
sagar.ingrails <sagar.ingrails@gmail.com>
2024-08-27 10:55:23 +0545

    feat:/contact

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   output: "export",
+  output: "export",
   env: {
     EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
   },
 };
 
