2f4adda374233e526ddecf6d8e5b4be3b1ec85e9
Sagar Adhikari <sagar.ingrails@gmail.com>
2025-01-12 17:08:21 +0545

    new changes built

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"VUS4NGCADeBlbi7unKb3NyvjUwz+uo8Qgm6mMx5T2fU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+ByJuWeNsWMJW0paBL1zBD5w/SWGzwCKxsapnhibTDE=\"}"
